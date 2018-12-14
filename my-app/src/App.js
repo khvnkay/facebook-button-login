0a91df5046bb6b454b9f15c8ae396fc0c8d4c66a
krongkann <krongkan@angstromsolutions.net>
2018-12-14 16:28:22 +0700

    add twwiter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  handleClick = () => {
+    axios
+      .post(
+        "https://api.twitter.com/oauth/authenticate?oauth_token=NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0",
+        {
+          firstName: "Fred",
+          lastName: "Flintstone"
+        }
+      )
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
   render() {
-    return <div className="App">Hello world </div>;
+    return (
+      <div className="App">
+        Hello world
+        <button onClick={this.handleClick}>login Twwiter</button>
+      </div>
+    );
   }
 }
 
